c0ad6c69325b2f723677b51cc8529a119606074f
chan48 <bobaa48@gmail.com>
2017-10-26 05:32:59 +0900

    up-171026-0532

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app = express(),
 path = require('path'),
 publicDir = path.join(__dirname,'public');
 
-app.use(express.static(publicDir))
-
+app.use(express.static(publicDir));
 app.listen(port);
+
+app.set('view engine', 'pug');
+app.set('views', './views');
