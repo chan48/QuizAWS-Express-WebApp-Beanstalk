3b458db1f69075dc61f5eb05423b2afbd8849c72
chan48 <bobaa48@gmail.com>
2017-10-27 22:02:31 +0900

    update on 2017/10/27 22:02:31

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var 
-app = express(),
-path = require('path'),
-publicDir = path.join(__dirname,'public');
-app.locals.pretty = true ;
+const express = require('express');
 
-app.use(express.static(publicDir));
+const 
+port = process.env.PORT || 3000,
+app = express(),
+path = require('path');
+app.locals.pretty = true ;
 app.listen(port);
+
+// const publicDir = path.join(__dirname,'public');
+// app.use(express.static(publicDir));
+
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', function(req, res){
   res.render('hi', {time:Date(), title:'Welcom to QuizAWS'});
+});
+
+app.get('/quiz-test', function(req, res){
+  res.render('hi');
 });
