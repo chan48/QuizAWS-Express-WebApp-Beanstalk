3db7931b07425a431b6418af0fbf06af217032c2
chan48 <bobaa48@gmail.com>
2017-11-03 18:55:28 +0900

    update on 2017/11/03 18:55:28

---
+++ index.js
@@ -0,0 +1,31 @@
+const 
+app = require('./server/server'),
+port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Listening');
+});
+
+
+
+// const express = require('express');
+// 
+// const 
+// port = process.env.PORT || 3000,
+// app = express(),
+// path = require('path');
+// app.locals.pretty = true ;
+// app.listen(port);
+// const publicDir = path.join(__dirname,'public');
+// app.use(express.static(publicDir));
+// 
+// app.set('view engine', 'pug');
+// app.set('views', './views');
+// 
+// app.get('/', function(req, res){
+  // res.render('hi', {time:Date(), title:'Welcom to QuizAWS'});
+// });
+// 
+// app.get('/quiz-test', function(req, res){
+  // res.render('hi');
+// });
